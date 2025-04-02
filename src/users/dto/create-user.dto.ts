f3a113538e05d31affeca1066890665fce84bec9
Leonardo Conti <eng.leonardoconti@gmail.com>
2025-04-02 18:38:21 -0300

    adding user methods

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { Roles } from '@prisma/client';
+
+export class CreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+  role: Roles;
+}
+// dto - data transfer object é um objeto que representa o corpo da requisição
+// this is a design pattern that is used to transfer data between different layers of the application
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -9,6 +17,7 @@ export class UsersController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
+    // o @Body aqui ja pega o corpo da requisição e transforma em um objeto javascript
     return this.usersService.create(createUserDto);
   }
 
@@ -19,16 +28,16 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
